c4b7fdc50651b1486d79e8b6392b2ba4b418f6b8
Quang Nguyen <quang.nguyen@adnx.com.vn>
2024-06-20 16:59:45 +0700

    initial nodejs base source twitter

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import databaseService from './services/database.services'
+import usersRouter from './routes/users.routes'
+import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
+import { config } from 'dotenv'
+import { UPLOAD_VIDEO_DIR } from './constants/dir'
+import staticRouter from './routes/static.routes'
+config()
+databaseService.connect()
+const app = express()
+const port = process.env.PORT || 4000
+// Tạo folder upload
+initFolder()
+app.use(express.json())
+app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
+app.use('/static', staticRouter)
+app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
+// app.use('/static', express.static(UPLOAD_IMAGE_DIR))
+app.use(defaultErrorHandler)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
