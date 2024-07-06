22367dfd0420a443f412b07d60cd1c6e78251856
Quang Nguyen <quang.nguyen@adnx.com.vn>
2024-07-06 16:19:07 +0700

    prepare refresh token

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
+import HTTP_STATUS from '~/constants/httpStatus'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
+import fs from 'fs'
 
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
   const result = await mediasService.uploadImage(req)
@@ -29,11 +31,36 @@ export const serveImageController = (req: Request, res: Response, next: NextFunc
   })
 }
 
-export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+export const serveVideoStreamController = async (req: Request, res: Response, next: NextFunction) => {
+  const range = req.headers.range
+  if (!range) {
+    return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header')
+  }
   const { name } = req.params
-  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
-    if (err) {
-      res.status((err as any).status).send('Not found')
-    }
-  })
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name)
+  // 1 MB=10^6 bytes (Tính theo hệ số 10, đây là thứ chúng ta thấy trên UI)
+  // Còn nếu tính theo hệ nhị phân thì 1 MB = 2^20 bytes (1024*1024)
+  // Dung lượng video
+  const videoSize = fs.statSync(videoPath).size
+  // Dung lượng video cho mỗi phân đoạn stream
+  const chunkSize = 30 * 10 ** 6 // 30MB
+  // Lấy giá trị byte bắt đầu từ header Range (vd: bytes=1048576-)
+  const start = Number(range.replace(/\D/g, ''))
+  // Lấy giá trị bytes kết thúc, vượt qúa dung lượng video thì lấy giá trị videoSize
+  const end = Math.min(start + chunkSize, videoSize - 1)
+
+  // Dung lượng thực tế cho mỗi đoạn video stream
+  // Thường đây sẽ là chunkSize, ngoại trừ đoạn cuối cùng
+  const contentLength = end - start + 1
+  const { default: mime } = await import('mime')
+  const contentType = mime.getType(videoPath) || 'video/*'
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+  res.writeHead(HTTP_STATUS.PARTIAL_CONTENT, headers)
+  const videoSteams = fs.createReadStream(videoPath, { start, end })
+  videoSteams.pipe(res)
 }
